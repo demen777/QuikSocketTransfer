7060fed54341744b07dfc5585b6bca607fe26025
demen <demen7@gmail.com>
2018-09-05 15:08:40 +0500

    Transfer from with resource to simple connect-close

--- quik_python_client/quik_socket_client.py
+++ quik_python_client/quik_socket_client.py
@@ -4,6 +4,10 @@ import sys
 import socket
 from io import BytesIO
 from typing import List, Any
+import logging
+
+
+logger = logging.getLogger(__name__)
 
 
 class ResponseError(Exception):
@@ -25,24 +29,39 @@ class QuikSocketClient:
         self.message_id = 0
         self.socket = None
 
-    def __enter__(self):
+    def connect(self):
+        logger.debug("Start connect")
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        logger.debug("connect before connect")
         self.socket.connect((self.host, self.port))
-        self.send_request('checkSecurity', [self.password])
-        return self
+        logger.debug("connect before checkSecurity")
+        self._send_request('checkSecurity', [self.password])
+        logger.debug("End connect")
 
-    def __exit__(self, exception_type, exception_value, traceback):
+    def close(self):
         if self.socket:
             self.socket.close()
 
     def send_request(self, command_name: str, args: List[Any]) -> List[Any]:
+        logger.debug("Start send_request")
+        self.connect()
+        res = self._send_request(command_name, args)
+        self.close()
+        logger.debug("End send_request")
+        return res
+
+    def _send_request(self,command_name, args):
         self.message_id += 1
         request_json = {"id": self.message_id, "method": command_name, "args": args}
         message_text = json.dumps(request_json)
+        logger.debug("_send_request before sendall")
         self.socket.sendall(message_text.encode('cp1251'))
+        logger.debug("_send_request after sendall")
         with BytesIO() as response_bytes:
             while True:
+                logger.debug("_send_request before recv")
                 fragment = self.socket.recv(8192)
+                logger.debug("_send_request after recv")
                 response_bytes.write(fragment)
                 if fragment[-1] == 10:
                     break
